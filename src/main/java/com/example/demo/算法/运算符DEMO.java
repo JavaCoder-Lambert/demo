b764758c89ecbfb53aa9cea3e57dd8d053fb3488
lizhijiang <724727634@qq.com>
2021-10-26 22:03:28 +0800

    JUC

---
+++ src/main/java/com/example/demo/算法/运算符DEMO.java
@@ -0,0 +1,15 @@
+package com.example.demo.算法;
+
+/**
+ * x<<y 相当于 x*（2的y次方） ；x>>y相当于x/2y
+ *     从计算速度上讲，移位运算要比算术运算快。
+ * @Author: lzj
+ * @Date: 2021/10/24 22:48
+ * @Description:
+ */
+public class 运算符DEMO {
+    public static void main(String[] args) {
+         System.out.println(4<<5);
+        System.out.println(4>>5);
+    }
+}
