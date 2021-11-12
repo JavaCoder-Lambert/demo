2a6c5d672778963600637c988c039767e7c76972
lizhijiang <724727634@qq.com>
2021-11-12 10:55:09 +0800

    代码提交

--- src/main/java/com/example/demo/netty/NettyDemo.java
+++ src/main/java/com/example/demo/netty/NettyDemo.java
@@ -4,13 +4,17 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.oio.OioServerSocketChannel;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author lizhijiang
@@ -19,36 +23,42 @@ import java.nio.charset.StandardCharsets;
  * @CreateTime 2021年11月11日 17:09
  */
 public class NettyDemo {
+    private static List<String> values;
+
     public static void server(int port) throws Exception {
         final ByteBuf buf = Unpooled.unreleasableBuffer(
                 Unpooled.copiedBuffer("Hi!\r\n", StandardCharsets.UTF_8));
-        EventLoopGroup group = new OioEventLoopGroup();
+        EventLoopGroup group = new NioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap();        //1
-
-            b.group(group)                                    //2
-                    .channel(OioServerSocketChannel.class)
+            //1
+            ServerBootstrap b = new ServerBootstrap();
+            //2
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
                     .localAddress(new InetSocketAddress(port))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {//3
+                    //3
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        public void initChannel(SocketChannel ch)
-                                throws Exception {
-                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {            //4
+                        public void initChannel(SocketChannel ch) {
+                            //4
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                                 @Override
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                    ctx.writeAndFlush(buf.duplicate()).addListener(ChannelFutureListener.CLOSE);//5
+                                    //5
+                                    ctx.writeAndFlush(buf.duplicate()).addListener(ChannelFutureListener.CLOSE);
                                 }
                             });
                         }
                     });
-            ChannelFuture f = b.bind().sync();  //6
+            //6
+            ChannelFuture f = b.bind().sync();
             f.channel().closeFuture().sync();
         } finally {
-            group.shutdownGracefully().sync();        //7
+            group.shutdownGracefully().sync();
         }
     }
 
     public static void main(String[] args) throws Exception {
-        server(16666);
+        List<String> values=new ArrayList<>();
     }
 }
