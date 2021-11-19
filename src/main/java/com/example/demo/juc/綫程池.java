10e18bede113017b76c1fca29c53c3ed75416230
lizhijiang <724727634@qq.com>
2021-11-20 01:12:56 +0800

    JUC

--- src/main/java/com/example/demo/juc/綫程池.java
+++ src/main/java/com/example/demo/juc/綫程池.java
@@ -1,9 +1,7 @@
 package com.example.demo.juc;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: lzj
@@ -15,6 +13,37 @@ public class 綫程池 {
             TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5)
             , Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("线程:" + Thread.currentThread().getName());
+            Integer num = DySchedule.getLine();
+            System.out.println("startline = " +(num-1000)+",endline = " + num);
+        }
+    }
 
+    public static class DySchedule {
+        private static AtomicInteger line = new AtomicInteger(0);
+        static ExecutorService pool = Executors.newFixedThreadPool(100);
+
+        public static int getLine(){
+            return line.addAndGet(1000);
+        }
+        public static void doJob(){
+            for (int i = 0;i<100;i++){
+                Thread thread = new MyThread();
+                pool.execute(thread);
+            }
+            pool.shutdown();
+
+        }
+        public static void main(String[] args) {
+            DySchedule.doJob();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
