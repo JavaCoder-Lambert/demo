c8af32264949da7b6b199e938c4f5f88ef1a6ae2
lizhijiang <724727634@qq.com>
2021-11-28 13:47:37 +0800

    JUC

--- src/main/java/com/example/demo/杂七杂八/DemoTest.java
+++ src/main/java/com/example/demo/杂七杂八/DemoTest.java
@@ -7,6 +7,6 @@ package com.example.demo.杂七杂八;
  */
 public class DemoTest {
     public static void main(String[] args) {
-        Person.builder().firstName("漏风小棉袄").jobName("打工仔").age(18).sex("female").build();
+        Person.builder().firstName("漏风小棉袄1").jobName("打工仔").age(18).sex("female").build();
     }
 }
