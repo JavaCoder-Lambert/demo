589fc10ae24253c909e184b5b0e4bb182c09b180
lizhijiang <724727634@qq.com>
2024-09-26 10:31:55 +0800

    111

--- src/main/java/com/example/demo/算法/DTWDemo.java
+++ src/main/java/com/example/demo/算法/DTWDemo.java
@@ -15,21 +15,22 @@ public class DTWDemo {
 
     public static void main(String[] args) {
         // 定义两个时间序列
-        double[] series1 = {1, 2, 3, 4, 5};
-        double[] series2 = {2, 3, 4, 5, 6, 7};
-
-        // 创建一个欧几里得距离计算器
-        EuclideanDistance euclideanDistance = new EuclideanDistance();
+        double[] series1 = {1,3,4,9,8,2,1,5,7,3};
+        double[] series2 = {1,3,1,4};
 
         // 创建一个DTW实例，使用欧几里得距离作为距离度量
-        Distance dtw = (x, y) -> 0;
-        DynamicTimeWarping DynamicTimeWarping=new DynamicTimeWarping(dtw);
+        Distance dtw = null;
+        DynamicTimeWarping<Object> DynamicTimeWarping=new DynamicTimeWarping<>(null);
+
 
         // 计算两个时间序列之间的DTW距离
-        double distance = dtw.d(series1, series2);
+        double distance = DynamicTimeWarping.d(series1, series2);
 
         // 输出DTW距离
         System.out.println("DTW distance between series1 and series2: " + distance);
+
+
+       // DynamicTimeWarping<Distance> DynamicTimeWarping1=new DynamicTimeWarping<Distance>();
     }
 
 }
